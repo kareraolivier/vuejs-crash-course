230a8a13e4258181bac4d0f3628934662fd13eb7
kareraolivier <kareraolivier@gmail.com>
2024-01-19 17:11:31 +0200

    feat: save data to local storage

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import Header from "./components/header.vue";
 import Balance from "./components/balance.vue";
 import IncomeExpence from "./components/incomeExpence.vue";
@@ -23,15 +23,18 @@ import TransactionList from "./components/transactionList.vue";
 import { Transaction } from "./components/types/interface";
 import { useToast } from "vue-toastification";
 const toast = useToast();
-const transactions = ref<Transaction[]>([
-  { id: 1, title: "pen", amount: -300 },
-  { id: 2, title: "salary", amount: 3000 },
-  { id: 3, title: "food", amount: -200 },
-  { id: 4, title: "banana", amount: -210 },
-  { id: 5, title: "math", amount: 0 },
-  { id: 6, title: "income", amount: 30000 },
-]);
+const transactions = ref<Transaction[]>([]);
 
+// get data from local storage
+onMounted(() => {
+  const savedTransactionString = localStorage.getItem("transaction");
+  if (savedTransactionString) {
+    const savedTransaction = JSON.parse(savedTransactionString);
+    if (savedTransaction) {
+      transactions.value = savedTransaction;
+    }
+  }
+});
 const total = computed(() =>
   transactions.value.reduce((acc, trans) => acc + trans.amount, 0)
 );
@@ -50,14 +53,24 @@ const expense = computed(() =>
 );
 
 const genelateUniqueId = () => Math.floor(Math.random() * 10000);
+
+//add transaction
 const handleSubmitedData = (submitData: any) => {
   transactions.value.unshift({ ...submitData, id: genelateUniqueId() });
+  savedTransactionToLocalStorage();
   toast.success("transactions completed");
 };
 
+//delete transaction
 const handleTransactionDelete = (id?: number) => {
   if (id) alert("do you want to delete this transaction?");
   transactions.value = transactions.value.filter((trans) => trans.id != id);
+  savedTransactionToLocalStorage();
   toast.success("delete completed");
+};
+
+//save to local storage
+const savedTransactionToLocalStorage = () => {
+  localStorage.setItem("transaction", JSON.stringify(transactions.value));
 };
 </script>
